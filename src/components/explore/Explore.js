4734a62d7e5bb66db1302a6e60ac8259081d40b6
Andre Felipe <afelipes@gmail.com>
2022-05-03 17:06:30 -0300

    Requisito 67 e 68

--- src/components/explore/Explore.js
+++ src/components/explore/Explore.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../Footer';
 import Header from '../header/Header';
+import styles from '../../css/Explorer.module.css';
 
 function Explore() {
   return (
     <>
       <Header title="Explore" />
+      <div className={ styles.container }>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="explore-foods"
+          >
+            Explore Foods
+
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="explore-drinks"
+          >
+            Explore Drinks
+
+          </button>
+        </Link>
+      </div>
       <Footer />
     </>
   );
