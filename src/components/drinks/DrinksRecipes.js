0501ff3f66361cb26eeb86e054d5a3d99597812c
lazaro <lazaroandriola@gmail.com>
2022-05-04 16:09:04 -0300

    Desenvolvida renderização dos ingredientes
    Co-authored-by: Ju-MM <Ju-MM@users.noreply.github.com>

--- src/components/drinks/DrinksRecipes.js
+++ src/components/drinks/DrinksRecipes.js
@@ -1,11 +1,14 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import RecipesContext from '../../context/RecipesContext';
+import shareIcon from '../../images/shareIcon.svg';
 import whiteHeart from '../../images/whiteHeartIcon.svg';
 import { requestRecipeDetail } from '../../services/API';
 import './drinksRecipes.css';
 
 function DrinksRecipes() {
+  const [ingredientsList, setIngredientsList] = useState([]);
+  const [measureList, setMeasureList] = useState([]);
   const { location: { pathname } } = useHistory();
   const { recipeDetail, setRecipeDetail } = useContext(RecipesContext);
   const detailsRecipeArray = Object.values(recipeDetail);
@@ -20,6 +23,32 @@ function DrinksRecipes() {
     requestDetails();
   }, []);
 
+  useEffect(() => {
+    if (detailsRecipeArray.length !== 0) {
+      setIngredientsList(Object.keys(recipeDetail.drinks[0]).reduce((acc, element) => {
+        if (element.includes('strIngredient')
+        && recipeDetail.drinks[0][element] !== null
+        && recipeDetail.drinks[0][element] !== '') {
+          const ingredient = recipeDetail.drinks[0][element];
+          acc = [...acc, ingredient];
+          return acc;
+        }
+        return acc;
+      }, []));
+
+      setMeasureList(Object.keys(recipeDetail.drinks[0]).reduce((acc, element) => {
+        if (element.includes('strMeasure')
+        && recipeDetail.drinks[0][element] !== null
+        && recipeDetail.drinks[0][element] !== '') {
+          const ingredient = recipeDetail.drinks[0][element];
+          acc = [...acc, ingredient];
+          return acc;
+        }
+        return acc;
+      }, []));
+    }
+  }, [recipeDetail]);
+
   return (
     <div>
       {
@@ -33,15 +62,25 @@ function DrinksRecipes() {
             />
             <h2 data-testid="recipe-title">{detailsRecipeArray[0][0].strDrink}</h2>
             <button type="button" data-testid="share-btn">
-              Share
+              <img src={ shareIcon } alt="share button" />
             </button>
             <button type="button" data-testid="favorite-btn">
               <img src={ whiteHeart } alt="favorite heart" />
             </button>
             <h5 data-testid="recipe-category">{detailsRecipeArray[0][0].strAlcoholic}</h5>
+            <h3>Ingredients</h3>
             <ul>
-              {/* colocar os ingredientes e quantidades aqui */}
-              <li data-testid="0-ingredient-name-and-measure" />
+              {
+                ingredientsList.map((element, index) => (
+                  <li
+                    data-testid={ `${index}-ingredient-name-and-measure` }
+                    key={ index }
+                  >
+                    { measureList[index] === undefined ? `${element}`
+                      : `${element} - ${measureList[index]}`}
+                  </li>))
+              }
+              {/* <li data-testid="0-ingredient-name-and-measure" /> */}
             </ul>
             <p data-testid="instructions">{detailsRecipeArray[0][0].strInstructions}</p>
             {/* Card de receitas de comidas recomendadas */}
--- src/components/foods/FoodsRecipes.js
+++ src/components/foods/FoodsRecipes.js
@@ -1,13 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import RecipesContext from '../../context/RecipesContext';
-import { requestRecipeDetail } from '../../services/API';
+import shareIcon from '../../images/shareIcon.svg';
 import whiteHeart from '../../images/whiteHeartIcon.svg';
+import { requestRecipeDetail } from '../../services/API';
 import './foodsRecipes.css';
 
 function FoodsRecipes() {
   const { location: { pathname } } = useHistory();
   const { recipeDetail, setRecipeDetail } = useContext(RecipesContext);
+  const [ingredientsList, setIngredientsList] = useState([]);
+  const [measureList, setMeasureList] = useState([]);
   const [youtubeEmbed, setYoutubeEmbed] = useState('');
   const detailsRecipeArray = Object.values(recipeDetail);
 
@@ -27,15 +30,42 @@ function FoodsRecipes() {
       const finalString = detailsRecipeArray[0][0].strYoutube
         .replace('watch?v=', 'embed/');
       setYoutubeEmbed(finalString);
+
+      setIngredientsList(Object.keys(recipeDetail.meals[0]).reduce((acc, element) => {
+        if (element.includes('strIngredient')
+          && recipeDetail.meals[0][element] !== null
+          && recipeDetail.meals[0][element] !== '') {
+          const ingredient = recipeDetail.meals[0][element];
+          acc = [...acc, ingredient];
+          return acc;
+        }
+        return acc;
+      }, []));
+
+      setMeasureList(Object.keys(recipeDetail.meals[0]).reduce((acc, element) => {
+        if (element.includes('strMeasure')
+          && recipeDetail.meals[0][element] !== null
+          && recipeDetail.meals[0][element] !== '') {
+          const ingredient = recipeDetail.meals[0][element];
+          acc = [...acc, ingredient];
+          return acc;
+        }
+        return acc;
+      }, []));
     }
   }, [recipeDetail]);
+
+  useEffect(() => {
+    console.log(ingredientsList);
+    console.log(measureList);
+  }, [ingredientsList, measureList]);
 
   return (
     <div className="container">
       {
         detailsRecipeArray.length === 1
         && (
-          <div>
+          <div className="container">
             <img
               src={ detailsRecipeArray[0][0].strMealThumb }
               alt={ detailsRecipeArray[0][0].strMeal }
@@ -43,15 +73,25 @@ function FoodsRecipes() {
             />
             <h2 data-testid="recipe-title">{detailsRecipeArray[0][0].strMeal}</h2>
             <button type="button" data-testid="share-btn">
-              Share
+              <img src={ shareIcon } alt="share button" />
             </button>
             <button type="button" data-testid="favorite-btn">
               <img src={ whiteHeart } alt="favorite heart" />
             </button>
             <h5 data-testid="recipe-category">{detailsRecipeArray[0][0].strCategory}</h5>
+            <h3>Ingredients</h3>
             <ul>
-              {/* colocar os ingredientes e quantidades aqui */}
-              <li data-testid="0-ingredient-name-and-measure" />
+              {
+                ingredientsList.map((element, index) => (
+                  <li
+                    data-testid={ `${index}-ingredient-name-and-measure` }
+                    key={ index }
+                  >
+                    { measureList[index] === undefined ? `${element}`
+                      : `${element} - ${measureList[index]}`}
+                  </li>))
+              }
+              {/* <li data-testid="0-ingredient-name-and-measure" /> */}
             </ul>
             <p data-testid="instructions">{detailsRecipeArray[0][0].strInstructions}</p>
             <iframe
