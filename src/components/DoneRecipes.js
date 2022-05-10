d18dbd3a814bdb78269aea38e38853c5ea7c51c6
Lazaro Andriola <lazaroandriola@gmail.com>
2022-05-10 16:47:50 -0300

    Requisito 57 concluido

--- src/components/DoneRecipes.js
+++ src/components/DoneRecipes.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import clipboardCopy from 'clipboard-copy';
 import { Link /* useHistory */ } from 'react-router-dom';
 import Header from './header/Header';
 import shareIcon from '../images/shareIcon.svg';
@@ -6,9 +7,18 @@ import './DoneRecipes.css';
 
 function DoneRecipes() {
   const [doneRecipe, setDoneRecipes] = useState([]);
+  const [shareClicked, setShareClicked] = useState(false);
+  const copy = clipboardCopy;
+
   useEffect(() => {
     setDoneRecipes(JSON.parse(localStorage.getItem('doneRecipes')));
   }, []);
+
+  function copyToClipBoard(type, id) {
+    copy(`http://localhost:3000/${type}s/${id}`);
+    setShareClicked(true);
+  }
+
   return (
     <div>
       <Header title="Done Recipes" />
@@ -30,25 +40,29 @@ function DoneRecipes() {
                 : `${element.alcoholicOrNot} - ${element.category}`}
             </p>
             <h5 data-testid={ `${index}-horizontal-name` }>{ element.name }</h5>
-            <p data-testid={ `${index}-horizontal-done-date` }>{ element.doneDate }</p>
-            <button type="button">
-              <img
-                data-testid={ `${index}-horizontal-share-btn` }
-                src={ shareIcon }
-                alt="shareIcon"
-              />
-            </button>
-            <div>
-              {element.tags.map((elementTag) => (
-                <p
-                  key={ elementTag }
-                  data-testid={ `${index}-${elementTag}-horizontal-tag` }
-                >
-                  {elementTag}
-                </p>
-              ))}
-            </div>
           </Link>
+          <p data-testid={ `${index}-horizontal-done-date` }>{ element.doneDate }</p>
+          <button
+            type="button"
+            onClick={ () => copyToClipBoard(element.type, element.id) }
+          >
+            <img
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ shareIcon }
+              alt="shareIcon"
+            />
+          </button>
+          {shareClicked && <p>Link copied!</p>}
+          <div>
+            {element.tags.map((elementTag) => (
+              <p
+                key={ elementTag }
+                data-testid={ `${index}-${elementTag}-horizontal-tag` }
+              >
+                {elementTag}
+              </p>
+            ))}
+          </div>
         </div>
       ))}
       {console.log(doneRecipe)}
