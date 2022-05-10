c4b48440d7a02ca74fd696876aede9bbd6259485
Lazaro Andriola <lazaroandriola@gmail.com>
2022-05-10 17:07:26 -0300

    Corrigido erro da pagina de receitas feitas

--- src/components/DoneRecipes.js
+++ src/components/DoneRecipes.js
@@ -11,7 +11,12 @@ function DoneRecipes() {
   const copy = clipboardCopy;
 
   useEffect(() => {
-    setDoneRecipes(JSON.parse(localStorage.getItem('doneRecipes')));
+    if (localStorage.getItem('doneRecipes') === null) {
+      localStorage.setItem('doneRecipes', JSON.stringify([]));
+    }
+    if (localStorage.getItem('doneRecipes') !== undefined) {
+      setDoneRecipes(JSON.parse(localStorage.getItem('doneRecipes')));
+    }
   }, []);
 
   function copyToClipBoard(type, id) {
@@ -25,46 +30,48 @@ function DoneRecipes() {
       <button type="button" data-testid="filter-by-all-btn">All</button>
       <button type="button" data-testid="filter-by-food-btn">Food</button>
       <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
-      {doneRecipe.length > 0 && doneRecipe.map((element, index) => (
-        <div key={ index }>
-          <Link to={ `/${element.type}s/${element.id}` }>
-            <img
-              className="doneRecipes"
-              src={ element.image }
-              alt={ element.name }
-              data-testid={ `${index}-horizontal-image` }
-            />
-            <p data-testid={ `${index}-horizontal-top-text` }>
-              { element.nationality !== ''
-                ? `${element.nationality} - ${element.category}`
-                : `${element.alcoholicOrNot} - ${element.category}`}
-            </p>
-            <h5 data-testid={ `${index}-horizontal-name` }>{ element.name }</h5>
-          </Link>
-          <p data-testid={ `${index}-horizontal-done-date` }>{ element.doneDate }</p>
-          <button
-            type="button"
-            onClick={ () => copyToClipBoard(element.type, element.id) }
-          >
-            <img
-              data-testid={ `${index}-horizontal-share-btn` }
-              src={ shareIcon }
-              alt="shareIcon"
-            />
-          </button>
-          {shareClicked && <p>Link copied!</p>}
-          <div>
-            {element.tags.map((elementTag) => (
-              <p
-                key={ elementTag }
-                data-testid={ `${index}-${elementTag}-horizontal-tag` }
-              >
-                {elementTag}
+      {doneRecipe.length > 0
+        && doneRecipe !== null
+        && doneRecipe.map((element, index) => (
+          <div key={ index }>
+            <Link to={ `/${element.type}s/${element.id}` }>
+              <img
+                className="doneRecipes"
+                src={ element.image }
+                alt={ element.name }
+                data-testid={ `${index}-horizontal-image` }
+              />
+              <p data-testid={ `${index}-horizontal-top-text` }>
+                { element.nationality !== ''
+                  ? `${element.nationality} - ${element.category}`
+                  : `${element.alcoholicOrNot} - ${element.category}`}
               </p>
-            ))}
+              <h5 data-testid={ `${index}-horizontal-name` }>{ element.name }</h5>
+            </Link>
+            <p data-testid={ `${index}-horizontal-done-date` }>{ element.doneDate }</p>
+            <button
+              type="button"
+              onClick={ () => copyToClipBoard(element.type, element.id) }
+            >
+              <img
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+                alt="shareIcon"
+              />
+            </button>
+            {shareClicked && <p>Link copied!</p>}
+            <div>
+              {element.tags.map((elementTag) => (
+                <p
+                  key={ elementTag }
+                  data-testid={ `${index}-${elementTag}-horizontal-tag` }
+                >
+                  {elementTag}
+                </p>
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       {console.log(doneRecipe)}
     </div>
   );
